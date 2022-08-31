0f170a3b64de2ebfead79b3662e8d31dc43415f6
Oscar Maia <oscarfgmaia@gmail.com>
2022-08-31 00:25:51 -0300

    1 layout de tela de criação de quizz de usuário finalizada - apenas layout.

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+//test to get request - working
+
+/* const promise = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes")
+promise.then(load)
+function load(promise){
+    console.log(promise.data)
+} */
